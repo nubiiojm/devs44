9d80ae8a597d7c9dd72f828debfecd9abc242a84
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-22 00:51:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,47 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "my-aws-tf-state-bucket11"
-    key    = "path/to/my/key"
+    key = "main"
     region = "ap-south-1"
+    dynamodb_table = "my-dynamo-db-table-11"
   }
 }
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::mybucket"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-      "Resource": "arn:aws:s3:::mybucket/path/to/my/key"
-    }
-  ]
-}
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:DescribeTable",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem"
-      ],
-      "Resource": "arn:aws:dynamodb:*:*:table/mytable"
-    }
-  ]
-}
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-state-prod"
-    key    = "network/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
-
-
