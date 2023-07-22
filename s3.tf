f11ffdeb9249a50341e3aa0bfe0ccc6750cc0fcd
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-22 20:12:56 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,22 @@
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
-  
-  tags = var.tags
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key ="AKIA3R2UVGLM5SXVRAUU"
+  secret_key ="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"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "venus3bucket1111"
+
+  tags = {
+    Name        = "My bucket"
+    }
 }
